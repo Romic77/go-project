207acdffbc92253511a5c87307aa0d0ec3c9b701
”rom1c77“ <280385556@qq.com>
2022-01-23 14:18:50 +0800

    目录结构调整

---
+++ project-01/unit3/test/util.go
@@ -0,0 +1,3 @@
+package test
+
+var StuNo int = 123
---
+++ project-01/unit3/unit3.go
@@ -0,0 +1,10 @@
+package main
+
+import "project-01/unit3/test"
+
+func main() {
+
+	//引入test包下的全局变量
+	var age int = test.StuNo
+	println(age)
+}
