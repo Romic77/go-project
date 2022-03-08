f6ba696582475c0a7f38d6e4d53a48e81c56e46c
”rom1c77“ <280385556@qq.com>
2022-03-08 14:24:48 +0800

    单元测试覆盖率 go test cover 单元测试基准测试 go test -bench=Split

--- project-02/split_test/split.go
+++ project-02/split_test/split.go
@@ -4,15 +4,15 @@ import (
 	"strings"
 )
 
-func split(s string, tmp string) []string {
+func split(s string, sep string) []string {
 	var str []string
-	index := strings.Index(s, tmp)
+	index := strings.Index(s, sep)
 	for index > 0 {
 		//append(str,s[:1] -> 0-i
 		str = append(str, s[:index])
 		//s[1+1:] 2->n
-		s = s[index+1:]
-		index = strings.Index(s, tmp)
+		s = s[index+len(sep):]
+		index = strings.Index(s, sep)
 	}
 	str = append(str, s)
 	return str
--- project-02/split_test/split_test.go
+++ project-02/split_test/split_test.go
@@ -5,19 +5,54 @@ import (
 	"testing"
 )
 
-// 通过 go test -v执行所有的单元测试
 func TestSplit(t *testing.T) {
-	result := split("abcd", "b")
-	want := []string{"a", "cd"}
-	if !reflect.DeepEqual(result, want) {
-		t.Errorf("want %#v, got %#v", want, result)
+	type testCase struct {
+		str  string
+		sep  string
+		want []string
+	}
+
+	testGroup := map[string]testCase{"case1": {
+		str:  "abcd",
+		sep:  "b",
+		want: []string{"a", "cd"},
+	},
+		"case2": {
+			str:  "abcd",
+			sep:  "bc",
+			want: []string{"a", "d"},
+		},
+		"case3": {
+			str:  "a:b:c:d",
+			sep:  ":",
+			want: []string{"a", "b", "c", "d"},
+		}}
+
+	/*for _, value := range testGroup {
+		gots := split(value.str, value.sep)
+		if !reflect.DeepEqual(value.want, gots) {
+			t.Fatalf("expect:%#v got:%#v\n", value.want, gots)
+		}
+	}*/
+
+	for key, value := range testGroup {
+		t.Run(key, func(t *testing.T) {
+			gots := split(value.str, value.sep)
+			if !reflect.DeepEqual(value.want, gots) {
+				t.Fatalf("expect:%#v got:%#v\n", value.want, gots)
+			}
+		})
 	}
 }
 
-func Test2Split(t *testing.T) {
-	result := split("abcd", "bc")
-	want := []string{"a1", "cd"}
-	if !reflect.DeepEqual(result, want) {
-		t.Errorf("want %#v, got %#v", want, result)
+//单元测试覆盖率是  go test -cover
+//单元测试覆盖率以文件生成 go test -cover -coverprofile=cover.out
+//单元测试覆盖率以html展示 go tool cover -html=cover.out
+
+//基准测试go test -bench=Split
+//go test -bench=Split -benchmem 查看申请内存
+func BenchmarkSplit(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		split("abcdef", "b")
 	}
 }
